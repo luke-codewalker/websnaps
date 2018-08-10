c4672997afdeaf12c8d30dd36f81d66e771c6afa
Luke Codewalker <arathorn089@arcor.de>
2018-08-11 01:25:39 +0200

    forgot about git and did a lot of stuff, all functionality now there started on design

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const video = document.querySelector(".stream.video");
+const video = document.querySelector(".video");
 const snapshots = document.querySelector(".snapshots");
 const filters = document.querySelector("#filters");
 const mirror = document.querySelector("#mirror");
@@ -8,30 +8,38 @@ let frontCam = false;
 let mirrored = mirror.checked ? -1 : 1;
 let filter = filters.value;
 
+const countdown = document.querySelector(".countdown");
+let secs = 3;
+countdown.textContent = secs;
+
 video.style.transform = `scaleX(${mirrored})`;
 video.style.filter = filter;
 
 startStreamingCamera({ video: { facingMode: "user" } });
 
-snapBtn.addEventListener("click", e => {
-  const canvas = document.createElement("canvas");
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
+video.addEventListener("click", takeSnapshot);
 
-  const context = canvas.getContext("2d");
-  context.filter = filter;
-  context.scale(mirrored, 1);
-  context.drawImage(
-    video,
-    -canvas.width / 2 + (canvas.width / 2) * mirrored,
-    0,
-    canvas.width,
-    canvas.height
-  );
+window.addEventListener("keydown", e => {
+  if (e.keyCode == 32) {
+    e.preventDefault();
+    takeSnapshot();
+  }
+});
 
-  canvas.addEventListener("click", downloadImg);
+snapBtn.addEventListener("click", () => {
+  snapBtn.setAttribute("disabled", "");
 
-  snapshots.appendChild(canvas);
+  const intervalID = setInterval(() => {
+    countdown.textContent = --secs;
+  }, 1000);
+
+  setTimeout(() => {
+    snapBtn.removeAttribute("disabled", "");
+    secs = 3;
+    countdown.textContent = secs;
+    clearInterval(intervalID);
+    takeSnapshot();
+  }, secs * 1000);
 });
 
 filters.addEventListener("change", e => {
@@ -62,14 +70,64 @@ function startStreamingCamera(constraints) {
     });
 }
 
-function downloadImg(e) {
+function takeSnapshot() {
+  const canvas = document.createElement("canvas");
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  const context = canvas.getContext("2d");
+  context.filter = filter;
+  context.scale(mirrored, 1);
+  context.drawImage(
+    video,
+    -canvas.width / 2 + (canvas.width / 2) * mirrored,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  canvas.classList.add("snapshot");
+  const wrapper = document.createElement("div");
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerHTML = "&times;";
+  deleteBtn.setAttribute("title", "Delete this snap!");
+  deleteBtn.classList.add("delete-btn", "btn");
+  deleteBtn.addEventListener("click", e => {
+    e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+  });
+
+  const downloadBtn = document.createElement("button");
+  downloadBtn.innerHTML = "💾";
+  downloadBtn.setAttribute("title", "Download this snap!");
+  downloadBtn.classList.add("download-btn", "btn");
+  downloadBtn.addEventListener("click", e =>
+    downloadImg(e.target.parentNode.querySelector("canvas"))
+  );
+
+  wrapper.addEventListener("mouseenter", () =>
+    wrapper.classList.add("focused")
+  );
+
+  wrapper.addEventListener("click", () => wrapper.classList.add("focused"));
+  wrapper.addEventListener("mouseleave", () =>
+    wrapper.classList.remove("focused")
+  );
+  wrapper.classList.add("snapshot-wrapper");
+  wrapper.appendChild(canvas);
+  wrapper.appendChild(deleteBtn);
+  wrapper.appendChild(downloadBtn);
+  snapshots.appendChild(wrapper);
+}
+
+function downloadImg(canvas) {
   const link = document.createElement("a");
   link.setAttribute(
     "style",
     "position:absolute; transform:translateY(-10000px);"
   );
   link.setAttribute("download", `snapshot-${Date.now()}.png`);
-  link.setAttribute("href", e.target.toDataURL("image/png"));
+  link.setAttribute("href", canvas.toDataURL("image/png"));
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
